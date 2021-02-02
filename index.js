a2d29ee35020ec5eb8aabca178d3ca27749b3355
Vishal Kumar Rathore <dragonblade2799@gmail.com>
2021-02-02 22:37:30 +0530

    input validation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "course 1" },
   { id: 2, name: "course 2" },
@@ -19,6 +21,20 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) res.status(404).send("course with given ID is not found");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    res.status(400).send("Name is required and should be min 3 chracter");
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
   res.send(course);
 });
 
